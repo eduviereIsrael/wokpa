4feacaa6a12094fdd09767279c59b277b9176a2a
eduviereisrael <eduviereisrael@gmail.com>
2023-10-18 15:06:46 +0100

    ui update

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -21,13 +21,13 @@ const PodcastCard = ({podcast}: Podcast) => {
     return title
   }
   return (
-    <div className='w-52 flex flex-col items-start justify-start bg-bg3 text-white   ' >
-      <img src={coverImage} alt="podcast image" className='w-full -m-0.5' />
-      <h5>{trimTitle(title)}</h5>
-      <p>By: {owner}</p>
-      <div>
-        <span>
-          <img src="/followcollection.svg" alt="" />
+    <div className='w-48 flex flex-col items-start justify-start bg-bg3 text-white p-0.5 pb-4 ' >
+      <img src={coverImage} alt="podcast image" className='w-full m-0 p-0 '  />
+      <h5 className='pl-2' >{trimTitle(title)}</h5>
+      <p className='pl-2  text-xs font-normal mt-3 ' >By: {owner}</p>
+      <div className='px-2 mt-auto flex items-center justify-between w-full' >
+        <span className='flex items-center justify-between mt-3 ' >
+          <img src="/followcollection.svg" alt="" className='' />
           {genre}
         </span>
         <img src="/gifticon.svg" alt="" />
